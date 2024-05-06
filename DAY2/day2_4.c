925f23a0f6eb50426753a3c75ddea4ae27132bbe
Yogesh Krishna Chavan <yogeshchavan113@gmail.com>
2024-05-06 17:08:17 +0530

    day2

---
+++ DAY2/day2_4.c
@@ -0,0 +1,34 @@
+//Escape Sequance \n \r \t \b \" \' %%
+
+#include<stdio.h>
+int main(){
+    //new line \n
+    printf("\nHello PH28");
+
+    //carriage return \r
+    printf("\nHello PH28 \rHiiee");
+
+    //tab \t
+    printf("\nHello\tPH28");
+    printf("\n\tHello");
+
+    //backspace \b
+    printf("\nHello PH28\b\b12");
+    printf("\nHello PH28\b\b  ");
+
+    //For printing \"
+    printf("\n\"Hello PH28\"");
+
+    //For printing \'
+    printf("\n\'Hello PH28\'");
+
+    //For printing %
+    printf("\nI got 80%");
+    printf("\nI got 80%%");
+
+    //For printing \n
+    printf("\\nHello PH28");
+
+
+    return 0;
+}
